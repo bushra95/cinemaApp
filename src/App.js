bf4bbe69ff0bc989d70d01e0cce26e351f0d4857
Bushra <77007669+bushra95@users.noreply.github.com>
2021-10-05 20:29:57 +0300

    mainNav

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import SimpleBottomNavigation from './components/MainNav';
+function App() {
+  return (
+    <>
+      <Header/>
+    <div className="app"></div>
+    <SimpleBottomNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <span className="header">Cinema</span>
+    )
+}
+export default Header
+
